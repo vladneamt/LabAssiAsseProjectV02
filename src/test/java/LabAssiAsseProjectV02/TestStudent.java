6590aab6fc8924d29a390b99ce8a9f2f2b67d26a
Vlad Ioan Neamt <nvie2185@scs.ubbcluj.ro>
2019-03-18 13:34:28 +0200

    Added mock test for student to check if we can make Jenkins work

---
+++ src/test/java/LabAssiAsseProjectV02/TestStudent.java
@@ -0,0 +1,11 @@
+package LabAssiAsseProjectV02;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TestStudent {
+    @Test
+    public void createStudentAllGood(){
+        assertTrue(true);
+    }
+}
